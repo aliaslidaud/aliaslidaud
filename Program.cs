1128fb91d69559155be456db4e814b830ac77270
aliaslidaud <147978179+aliaslidaud@users.noreply.github.com>
2024-02-12 23:21:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> vagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int capacity = int.Parse(Console.ReadLine());
+
+            string comand = Console.ReadLine();
+            while (comand != "end")
+            {
+                string[] nextAction = comand.Split();
+                if (nextAction.Length == 1)
+                {
+                    int passengers = int.Parse(nextAction[0]);
+                    for (int i = 0; i < vagons.Count; i++)
+                    {
+                        if ((capacity - vagons[i]) < passengers)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            vagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    int vagonsAdding = int.Parse(nextAction[1]);
+                    vagons.Add(vagonsAdding);
+                }
+                comand = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", vagons));
+        }
+    }
+}
